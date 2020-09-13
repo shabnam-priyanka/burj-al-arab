b6d5f3a89be5121009eccf5c2b2b3b2f42d3b599
shabnam-priyanka <66578322+shabnam-priyanka@users.noreply.github.com>
2020-09-13 17:49:33 -0400

    done

---
+++ src/components/Login/Login.js
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import {userContext} from '../../App'
+import { useHistory, useLocation } from 'react-router-dom';
+
+const Login = () => {
+        const [loggedInUser, setLoggedInUser] = useContext(userContext);
+        const history = useHistory();
+        const location = useLocation();
+        let { from } = location.state || { from: { pathname: "/" } };
+
+    
+        // const googleSignIn = () => {
+        if(firebase.apps.length === 0){
+            firebase.initializeApp(firebaseConfig);
+        }
+        
+        
+        const googleSignIn = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // The signed-in user info.
+            const {displayName, email} = result.user;
+            const signedInUser = {name: displayName,email };
+            setLoggedInUser(signedInUser)
+            history.replace(from);
+            // ...
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+
+
+    }
+    
+    
+    
+    return (
+        <div>
+            <h1>This is Login</h1>
+            <button onClick={googleSignIn}>Google Sign in  </button>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDLoLXomUOJUPz6y8tjc1lG5Lm6cpAZvkg",
+    authDomain: "burj-al-arab-4ca67.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-4ca67.firebaseio.com",
+    projectId: "burj-al-arab-4ca67",
+    storageBucket: "burj-al-arab-4ca67.appspot.com",
+    messagingSenderId: "266227848979",
+    appId: "1:266227848979:web:a7c253fd9b99fa3f42ace6"
+  };
+
+export default firebaseConfig;
